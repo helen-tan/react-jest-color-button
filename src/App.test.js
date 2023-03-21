adc78172a9b063c03fb5ce60dfff401cc4030f8a
helen-tan <helentanmz0909@gmail.com>
2023-03-21 11:14:01 +0800

    Wrote test to check if button has correct initial color

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+test('button has correct initial color', () => {
+  // 1. Render component (in virtual DOM)
+  render(<App/>)
+  // 2. Find element we want - role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' })
+  // 3. Assertion: Expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+});
+
+
+
+test('button turns blue when clicked', () => {
+
 });
