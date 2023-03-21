ef371efa04cf11da167557b6bc027d8abe5c90b6
helen-tan <helentanmz0909@gmail.com>
2023-03-21 11:20:47 +0800

    logRoles method for seeing element roles available in a component

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { logRoles } from '@testing-library/react';
 import App from './App';
 
 // test('renders learn react link', () => {
@@ -9,8 +10,12 @@ import App from './App';
 
 
 test('button has correct initial color', () => {
+  // If you want to know the roles in <App/> destructure it into a container and run logRoles on it
+  // const { container } = render(<App/>) 
+  // logRoles(container)
+
   // 1. Render component (in virtual DOM)
-  render(<App/>)
+  render(<App/>) 
   // 2. Find element we want - role of button and text of 'Change to blue'
   const colorButton = screen.getByRole('button', { name: 'Change to blue' })
   // 3. Assertion: Expect the background color to be red
